c169eacc65cdbd9783e603f19a0164cb6a9a848f
Umbar557 <101636011+Umbar557@users.noreply.github.com>
2025-07-11 09:31:08 +0700

    utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+def calculate_atr(df, period=14):
+    df['H-L'] = df['high'] - df['low']
+    df['H-PC'] = abs(df['high'] - df['close'].shift(1))
+    df['L-PC'] = abs(df['low'] - df['close'].shift(1))
+    tr = df[['H-L', 'H-PC', 'L-PC']].max(axis=1)
+    atr = tr.rolling(period).mean()
+    return atr.iloc[-1]
